85233a5054f6e9cf6b12acc2d7081ddb11e441bd
shubh <shubhamdixit863@gmail.com>
2023-08-26 20:24:29 +0530

    node questions

---
+++ data.js
@@ -0,0 +1,8 @@
+function foo(){
+    console.log("hello world");
+}
+
+foo();
+
+// Write a function to calculate the multiplication of five numbers
+// Write a function to calculate the simple interest 
