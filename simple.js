4da83393aa19478f7edf45909c814da8ce740bf8
shubh <shubhamdixit863@gmail.com>
2023-08-27 20:29:00 +0530

    questions on modules

--- simple.js
+++ simple.js
@@ -18,3 +18,19 @@ const h="ganesh";
 module.exports={
     h
 }
+
+
+/*
+
+1. **Basic Importing**:
+   - You have a file named `mathTools.js` that exports a function named `add`. How would you import the `add` function into another file called `app.js`?
+
+2. **Exporting Multiple Functions**:
+   - You've written two functions in `stringTools.js`: `toUpperCase` and `toLowerCase`. How would you export both functions so they can be imported into another module?
+
+3. **Using Built-in Modules**:
+   - Which built-in Node.js module allows you to interact with the file system, and how would you use it to read the content of a file named `data.txt`?
+
+4. **Module Properties**:
+   - Every module in Node.js has access to an object named `module`. Which property of this object provides information about the current module's file, and how would you use it to log the filename (without the full path) of the current module?
+*/
