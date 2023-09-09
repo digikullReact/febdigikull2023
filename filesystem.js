d3e3d887dde60aee0708df5ae051aeb97d459b63
shubh <shubhamdixit863@gmail.com>
2023-09-09 19:36:31 +0530

    fs with promises

--- filesystem.js
+++ filesystem.js
@@ -1,4 +1,6 @@
 const fs=require("fs");
+const fsPromise=require("fs/promises");
+const os=require("os");
 /*
 fs.readFile("/Users/shubhpro/Desktop/rec.mov","utf-8",(err,data)=>{
     if (err){
@@ -8,7 +10,6 @@ fs.readFile("/Users/shubhpro/Desktop/rec.mov","utf-8",(err,data)=>{
     console.log(data);
 
 })
-*/
 fs.writeFile("demo.txt","hello world",(err,data)=>{
     if (err){
         console.log("Errror",err);
@@ -17,5 +18,84 @@ fs.writeFile("demo.txt","hello world",(err,data)=>{
     console.log("File Written successfully");
 })
 
+fs.appendFile("demo.txt","hello world"+os.EOL,(err,data)=>{
+    if (err){
+        console.log("Errror",err);
+        return;
+    }
+    console.log("File Written successfully");
+})
 
-console.log("hey there");
+fs.unlink("demo.txt",(err,data)=>{
+
+    if (err){
+        console.log("Errror",err);
+        return;
+    }
+    console.log("File Deleted successfully");
+
+})
+
+*/
+// Promise version---->
+
+// Callback to promises
+
+
+
+
+function ReadFilePromise(fileName){
+    const promise=new Promise((res,rej)=>{
+
+        fs.readFile(fileName,"utf-8",(err,data)=>{
+            if (err){
+                rej(err)
+            }
+            res(data);
+        
+        })
+
+
+    })
+  return promise;
+}
+/*
+ReadFilePromise("data.txt").then(data=>{
+    console.log(data);
+
+}).catch(err=>{
+    console.log(err);
+})
+*/
+
+fsPromise.writeFile("data.txt","hello world").then(data=>{
+   // console.log("hey-----",data);
+}).catch(err=>{
+    console.log(err);
+})
+
+setTimeout(()=>{
+    fsPromise.readFile("data.txt","utf-8").then(data=>{
+        console.log("the data",data);
+    
+    }).catch(err=>{
+        console.log(err);
+    })
+
+},1000)
+
+
+
+fsPromise.appendFile("data.txt","hello world"+os.EOL).then(data=>{
+    console.log(data);
+
+}).catch(err=>{
+    console.log(err);
+})
+
+fsPromise.unlink("data.txt").then(data=>{
+    console.log(data);
+
+}).catch(err=>{
+    console.log(err);
+})
