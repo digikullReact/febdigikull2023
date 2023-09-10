7417df1df714570753764d6702060b2d33f7f938
shubh <shubhamdixit863@gmail.com>
2023-09-10 19:33:02 +0530

    event objects

--- events.js
+++ events.js
@@ -1,9 +1,14 @@
 const EventEmitter=require("events");
+const fs= require("fs");
 const eventObject=new EventEmitter();
 
 // Registered the listeners
 eventObject.on("fileRead",(data)=>{
     console.log("Event emitted----",data);
+    for (let i=0;i<data.length;i++){
+        fs.writeFileSync(data[i],"data created");
+
+    }
     // You can create those files even --
     // or you can d
 })
