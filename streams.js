10f4d7f7b353629270777fdf9c9b68f6b74fd8c3
shubh <shubhamdixit863@gmail.com>
2023-09-15 22:02:54 +0530

    node js

--- streams.js
+++ streams.js
@@ -15,9 +15,15 @@ const ws =fs.createWriteStream("largecopy.txt");
 
 
 rs.pipe(ws);
-
+let counter=0;
 rs.on("data",(data)=>{
-    console.log(data.toString());
+    // Write in the file here using after converting to uppercase fs.append()
+    counter++;
+  //  console.log(data.toString());
+})
+
+rs.on("end",()=>{
+    console.log(counter);
 })
 
 // Read streams
@@ -30,7 +36,7 @@ rs.on("data",(data)=>{
  * converts its content to upper case
  * and then writes to to the uppercase.txt
  * 
- * 2- Write a Node js progra, that reads content and every time the data event is fired
+ * 2- Write a Node js program, that reads content and every time the data event is fired
  * you have to count the chunks recevd and print the total number of chunks
  * 
  */
