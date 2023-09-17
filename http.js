222a89b1f90b66575bb2f70935c5a6e2efd0eb4e
shubh <shubhamdixit863@gmail.com>
2023-09-17 19:16:51 +0530

    crud operation

--- http.js
+++ http.js
@@ -1,5 +1,6 @@
 const http=require("http");
 const fs=require("fs");
+const os=require("os");
 
 const listener=(req,res)=>{
     console.log(req.method);
@@ -36,8 +37,37 @@ const listener=(req,res)=>{
                 break;
          }
 
-    }else{
-        res.end("Method Not supported");
+    }else if(req.method=="POST"){
+        switch (req.url) {
+            case "/data":
+                 let body="";
+                 req.on("data",(chunk)=>{
+                    body=body+chunk;
+                 })
+
+                 req.on("end",()=>{
+                    //console.log(body.toString());
+                    const obj=JSON.parse(body.toString());
+                    fs.appendFile("db.json",body.toString()+os.EOL,(err,data)=>{
+                        res.end("Data route");
+
+                    })
+                   
+                 })
+
+    
+                
+                break;
+            
+
+              
+     
+         
+            default:
+                res.end("404 Not Found")
+    
+                break;
+         }
     }
      
    
@@ -48,3 +78,10 @@ const server=http.createServer(listener);
 server.listen(8090,()=>{
     console.log("Server Started listening on port ",8090)
 })
+
+
+/**
+ * You have to write the append api 
+ * such that it only takes unique records 
+ * you have to check for unique email
+ */
