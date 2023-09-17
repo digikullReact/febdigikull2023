71bfc647cea78371e36f44046356c611b851030c
shubh <shubhamdixit863@gmail.com>
2023-09-17 20:03:08 +0530

    Email adding to the data

--- http.js
+++ http.js
@@ -47,11 +47,45 @@ const listener=(req,res)=>{
 
                  req.on("end",()=>{
                     //console.log(body.toString());
-                    const obj=JSON.parse(body.toString());
-                    fs.appendFile("db.json",body.toString()+os.EOL,(err,data)=>{
-                        res.end("Data route");
+                    const obj2=JSON.parse(body.toString());
 
+                    fs.readFile("db.json","utf-8",(err,data)=>{
+                        if (err){
+                            res.end("Error reading file");
+                            return
+                        }
+
+                        if(data.length>0){
+                            // data is there 
+                            const obj=JSON.parse(data);
+
+                            // IF the email is already there 
+
+                             const filetred=obj.find(ele=>ele.email==obj2.email);
+                             if (!filetred){
+                                console.log(obj);
+                            obj.push(JSON.parse(body.toString()))
+                            const writeData=JSON.stringify(obj)
+                            fs.writeFile("db.json",writeData,(err,data)=>{
+                                res.end("Data  Added");
+        
+                            })
+                               return;
+                             }
+
+                             res.end("Email already esists");
+
+                        }else{
+                            const writeData=JSON.stringify([JSON.parse(body.toString())])
+                            fs.appendFile("db.json",writeData,(err,data)=>{
+                                res.end("Data Added");
+        
+                            })
+                        }
                     })
+
+
+                 
                    
                  })
 
