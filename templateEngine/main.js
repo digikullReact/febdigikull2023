ac4a102dd236d6ead869065539b3114f64b537ee
shubh <shubhamdixit863@gmail.com>
2023-09-30 11:36:45 +0530

    Ecommerce page

--- templateEngine/main.js
+++ templateEngine/main.js
@@ -36,6 +36,27 @@ app.get("/list",(req,res)=>{
     res.render("list",{result:data})
 
 })
+/**
+ * Create a route ,read the json file
+ * render its data in the form of bootstrap table
+ */
+app.get("/table",(req,res)=>{
+       // Firstly read the file 
+       fs.readFile("db.json","utf-8",(err,data)=>{
+        if(err){
+            res.send("Error in reading data");
+            return;
+        }
+        res.render("table",{result:JSON.parse(data)})
+    })
+    
+
+})
+/**
+ * Call -https://fakestoreapi.com/products
+ * And render the products in template engine using this
+ * template -https://getbootstrap.com/docs/5.3/examples/album/
+ */
 app.listen(5050,()=>{
     console.log("Server Running at port ",5050)
 })
