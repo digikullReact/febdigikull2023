6f952670b84425261cb1e5f380dcf85066d752c2
shubh <shubhamdixit863@gmail.com>
2023-10-28 12:07:26 +0530

    file upload

--- dbcrud/app.js
+++ dbcrud/app.js
@@ -3,7 +3,9 @@ const app=express();
 const mongorouter=require("./routes/mongodbcrud");
 const mysqlrouter=require("./routes/mysqlcrud");
 const uploadRouter=require("./routes/fileUpload");
+const cors=require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/mongodbcrud",mongorouter);
